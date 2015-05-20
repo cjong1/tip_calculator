bf584d8684337aed17aba74d42193962e08e035f
Courtney Jong <courtney.jong@gmail.com>
2015-05-20 16:49:28 -0700

    first commit

--- tipster/ViewController.swift
+++ tipster/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var groupAmount: UILabel!
+    
+    var groupSize = 1
+    var tipPercentages = [0.18, 0.2, 0.22]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "0.00"
+        totalLabel.text = "0.00"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        reload()
+    }
 
+    @IBAction func groupSlider(sender: UISlider) {
+        groupSize = Int(sender.value)
+        groupAmount.text = "\(groupSize)"
+        reload()
+    }
+    
+    func reload() {
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = (billAmount + tip)/Double(groupSize)
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
